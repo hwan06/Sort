553199c5956f224e31cff8fd925053a5de1cbcb2
최환 <114748934+hwan06@users.noreply.github.com>
2023-03-07 10:12:41 +0900

    Add files via upload

---
+++ Insertion_Sort.java
@@ -0,0 +1,25 @@
+package dd;
+
+import java.util.Arrays;
+
+public class Insertion_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 3, 4, 1, 2, 5 }; // 배열방
+		
+		System.out.println("기본 배열방: " + Arrays.toString(arr));
+		for (int i = 1; i < arr.length; i++) { // 배열방 길이-1 만큼 반복
+			for (int j = i; j > 0; j--) { // 배열방 번호
+
+				if (arr[j - 1] > arr[j]) { // 비교할 값의 크기가 뒤에있는 방의 값의 크기보다 작은지 검사
+					int temp = arr[j - 1]; // temp에 비교할 값보다 큰 값을 넣기
+					arr[j - 1] = arr[j]; // 큰 값이 있었던 자리에 작은 값 넣기
+					arr[j] = temp; // 작은 값이 있었던 자리에 큰값(temp) 넣기
+				}
+			}
+		}
+		System.out.println("정렬된 배열방: "+Arrays.toString(arr)); // 정렬 출력
+	}
+}
